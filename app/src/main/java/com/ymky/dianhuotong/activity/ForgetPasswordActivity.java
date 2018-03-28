736dfece083327ece919eb18faeb494fc5bc2b7f
Administrator <lizhetongliao@163.com>
2018-03-28 10:00:58 +0800

    3.28-1提交

---
+++ app/src/main/java/com/ymky/dianhuotong/activity/ForgetPasswordActivity.java
@@ -0,0 +1,28 @@
+package com.ymky.dianhuotong.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ymky.dianhuotong.R;
+import com.ymky.dianhuotong.custom.DiaHuiTongBaseTitleBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+    @BindView(R.id.forgetpwd_titlebar)
+    DiaHuiTongBaseTitleBar diaHuiTongBaseTitleBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+        ButterKnife.bind(this);
+        inIt();
+    }
+
+    private void inIt() {
+        diaHuiTongBaseTitleBar.setCenterTextView("忘记密码");
+        diaHuiTongBaseTitleBar.setLeftImage(R.drawable.icon_back);
+    }
+}
