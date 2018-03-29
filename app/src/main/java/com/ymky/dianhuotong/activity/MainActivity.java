aaf9dd34117cbe11612a4afcd46faff8b596a4bc
Administrator <lizhetongliao@163.com>
2018-03-29 14:08:48 +0800

    329-4

--- app/src/main/java/com/ymky/dianhuotong/activity/MainActivity.java
+++ app/src/main/java/com/ymky/dianhuotong/activity/MainActivity.java
@@ -1,8 +1,17 @@
 package com.ymky.dianhuotong.activity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.ymky.dianhuotong.R;
 import com.ymky.dianhuotong.custom.DiaHuiTongBaseTitleBar;
@@ -15,13 +24,22 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements MainIF{
+public class MainActivity extends AppCompatActivity implements MainIF,DrawerLayout.DrawerListener{
     @BindView(R.id.drawer_listview)
     ListView listView;
     @BindView(R.id.mian_titlebar)
     DiaHuiTongBaseTitleBar diaHuiTongBaseTitleBar;
+    @BindView(R.id.mian_drawer)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.main_relativelayout_root)
+    RelativeLayout relativeLayout;
+    @BindView(R.id.drawer_linearlayout)
+    LinearLayout linearLayout;
+    boolean isOpenDrawer = false;
     private MainActivityPrecenter mainActivityPrecenter;
     private DrawerLayoutAdapter drawerLayoutAdapter;
+    private WindowManager windowManager;
+    private Display display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,13 +51,47 @@ public class MainActivity extends AppCompatActivity implements MainIF{
     private void inIt(){
         diaHuiTongBaseTitleBar.setLeftImage(R.drawable.icon_go_personal);
         diaHuiTongBaseTitleBar.setCenterTextView(getString(R.string.app_name));
+        diaHuiTongBaseTitleBar.setLeftOnclickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isOpenDrawer){
+                    drawerLayout.openDrawer(Gravity.START);
+                }else {
+                    drawerLayout.closeDrawers();
+                }
+
+            }
+        });
         mainActivityPrecenter=new MainActivityPrecenter(this,this);
         mainActivityPrecenter.getlistData();
+        windowManager=(WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        display=windowManager.getDefaultDisplay();
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
     }
 
     @Override
     public void updateListview(int[] array, ArrayList<String> list) {
         drawerLayoutAdapter=new DrawerLayoutAdapter(list,array,this);
         listView.setAdapter(drawerLayoutAdapter);
+    }
+
+    @Override
+    public void onDrawerSlide(View drawerView, float slideOffset) {
+                //relativeLayout.layout();
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+                isOpenDrawer = true;
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+                isOpenDrawer = false;
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+
     }
 }
