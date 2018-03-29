3d9c0ee79162e59113162517a2129f345d701d40
Administrator <lizhetongliao@163.com>
2018-03-29 09:43:56 +0800

    329-2

---
+++ app/src/main/java/com/ymky/dianhuotong/person/PersonInfmoActivity.java
@@ -0,0 +1,15 @@
+package com.ymky.dianhuotong.person;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ymky.dianhuotong.R;
+
+public class PersonInfmoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_infmo);
+    }
+}
