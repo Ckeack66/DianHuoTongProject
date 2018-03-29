3b14ddaa30c81ad31666d454907cfdd4d5867e10
Administrator <lizhetongliao@163.com>
2018-03-29 17:42:29 +0800

    329-6

---
+++ GridViewLine/src/main/java/com/jake/library/GridViewLine.java
@@ -0,0 +1,58 @@
+package com.jake.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.GridView;
+
+public class GridViewLine extends GridView {
+    private float lineWidth = 1;
+    private int lineColor = Color.GRAY;
+
+    public GridViewLine(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public GridViewLine(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public GridViewLine(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attr) {
+        if (attr != null) {
+            TypedArray array = context.obtainStyledAttributes(attr, R.styleable.GridViewLine);
+            lineColor = array.getColor(R.styleable.GridViewLine_line_color, Color.GRAY);
+            lineWidth = array.getDimension(R.styleable.GridViewLine_line_width, 5);
+        }
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+        int childCount = getChildCount();
+        Paint paint = new Paint();
+        paint.setStrokeWidth(lineWidth);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(lineColor);
+        for (int i = 0; i < childCount; i++) {
+            View cellView = getChildAt(i);
+            if (i == 0 | i == 3 | i == 6 | i == 1 | i == 4 | i == 7) {
+                canvas.drawLine(cellView.getRight(), cellView.getTop(), cellView.getRight(), cellView.getBottom(), paint);
+            }
+            if (i != 6 &&i != 7 && i != 8) {
+                canvas.drawLine(cellView.getLeft(), cellView.getBottom(), cellView.getRight(), cellView.getBottom(), paint);
+            }
+
+        }
+    }
+}
