7ed3c2616aab9512642aafd9ee4e6c2ea5e9f628
Administrator <lizhetongliao@163.com>
2018-04-01 15:10:55 +0800

    4-1-4

--- app/src/main/java/com/ymky/dianhuotong/activity/MainActivity.java
+++ app/src/main/java/com/ymky/dianhuotong/activity/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends BaseActivity implements MainIF, DrawerLayout.D
     protected void onResume() {
         super.onResume();
         if (BaseApplication.getInstansApp().getToakens() != null) {
-            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
         }
     }
 
