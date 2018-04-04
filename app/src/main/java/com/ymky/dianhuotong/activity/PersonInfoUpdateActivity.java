08c703d8f3d2d69a5fd8ea2808147ae6341b08a0
Administrator <lizhetongliao@163.com>
2018-04-04 18:11:53 +0800

    4-4-6

--- app/src/main/java/com/ymky/dianhuotong/activity/PersonInfoUpdateActivity.java
+++ app/src/main/java/com/ymky/dianhuotong/activity/PersonInfoUpdateActivity.java
@@ -1,12 +1,18 @@
 package com.ymky.dianhuotong.activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.joker.api.Permissions4M;
+import com.joker.api.wrapper.Wrapper;
 import com.ymky.dianhuotong.R;
 import com.ymky.dianhuotong.base.BaseTool;
 import com.ymky.dianhuotong.custom.ToastUtil;
@@ -79,7 +85,82 @@ public class PersonInfoUpdateActivity extends TakePhotoActivity {
     @OnClick(R.id.person_info_update_select_message)
     void goSelectPhoto() {
         //getTakePhoto().onPickFromGallery();
-        getTakePhoto().onPickFromCapture(BaseTool.createImagePathUri(this));
+
+        Permissions4M.get(this)
+                .requestPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .requestCodes(1001, 1002)
+                .requestListener(new Wrapper.PermissionRequestListener() {
+                    @Override
+                    public void permissionGranted(int code) {
+                        switch (code) {
+                            case 1001:
+                                ToastUtil.makeText(mContext, "读权限申请成功！", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1002:
+                                ToastUtil.makeText(mContext, "读权限申请成功！", Toast.LENGTH_SHORT).show();
+                                getTakePhoto().onPickFromCapture(BaseTool.createImagePathUri(mContext));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void permissionDenied(int code) {
+                        switch (code) {
+                            case 1001:
+                                ToastUtil.makeText(mContext, "读权限申请失败！", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1002:
+                                ToastUtil.makeText(mContext, "写权限申请失败！", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void permissionRationale(int code) {
+                        switch (code) {
+                            case 1001:
+                                ToastUtil.makeText(mContext, "请打开读权限！", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1002:
+                                ToastUtil.makeText(mContext, "请打开写权限！", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                })
+                .requestCustomRationaleListener(new Wrapper.PermissionCustomRationaleListener() {
+                    @Override
+                    public void permissionCustomRationale(int code) {
+                        switch (code) {
+                            case 1001:
+                                ToastUtil.makeText(mContext, "请打开写权限！", Toast.LENGTH_SHORT).show();
+                                new AlertDialog.Builder(mContext)
+                                        .setMessage("地理位置权限权限申请：\n我们需要您开启地理位置权限(in fragment with " +
+                                                "annotation)")
+                                        .setPositiveButton("确定", new DialogInterface
+                                                .OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Permissions4M.get((Activity) mContext)
+                                                        .requestOnRationale()
+                                                        .requestPermissions(Manifest.permission
+                                                                .ACCESS_FINE_LOCATION)
+                                                        .requestCodes(1001)
+                                                        .request();
+                                            }
+                                        })
+                                        .show();
+                                break;
+                        }
+
+                    }
+                })
+                .request();
     }
 
     private void inIt() {
